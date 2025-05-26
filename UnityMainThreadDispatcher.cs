5d36178c344db4a126e92f452bbce4c25bacc086
Mukul Tripathi <166616841+Mukul1801@users.noreply.github.com>
2025-05-26 13:40:42 +0530

    Create UnityMainThreadDispatcher.cs

---
+++ UnityMainThreadDispatcher.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A Unity component to dispatch actions to the main Unity thread.
+/// This is essential for Android callbacks that come from native threads.
+/// </summary>
+public class UnityMainThreadDispatcher : MonoBehaviour
+{
+    private static UnityMainThreadDispatcher _instance;
+    private readonly Queue<Action> _executionQueue = new Queue<Action>();
+
+    public static UnityMainThreadDispatcher Instance()
+    {
+        if (_instance == null)
+        {
+            // Try to find existing instance
+            _instance = FindObjectOfType<UnityMainThreadDispatcher>();
+            
+            if (_instance == null)
+            {
+                // Create new instance
+                GameObject go = new GameObject("UnityMainThreadDispatcher");
+                _instance = go.AddComponent<UnityMainThreadDispatcher>();
+                DontDestroyOnLoad(go);
+            }
+        }
+        return _instance;
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Enqueue(Action action)
+    {
+        if (action == null)
+            return;
+
+        lock (_executionQueue)
+        {
+            _executionQueue.Enqueue(action);
+        }
+    }
+
+    void Update()
+    {
+        lock (_executionQueue)
+        {
+            while (_executionQueue.Count > 0)
+            {
+                try
+                {
+                    _executionQueue.Dequeue().Invoke();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError("Error executing queued action: " + e.Message + "\n" + e.StackTrace);
+                }
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
+}
